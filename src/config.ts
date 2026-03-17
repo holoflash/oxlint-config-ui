08279a47fb2885cb5bc175c0d1cf4cbda8c44e2d
Redas Jefisovas <114405740+holoflash@users.noreply.github.com>
2026-03-17 19:51:36 +0100

    chore: update dependencies

--- src/config.ts
+++ src/config.ts
@@ -55,8 +55,8 @@ export const INITIAL_STATE = {
 } as const;
 
 export const VERSIONS = {
-  OXLINT: "1.51.0",
-  TSGOLINT: "0.15.0",
+  OXLINT: "1.56.0",
+  TSGOLINT: "0.17.0",
 } as const;
 
 export const ANSI = {
